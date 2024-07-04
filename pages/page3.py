892678e37c3e9be055e2c995cc1d93eca5298fe1
akathegod <61387154+akathegod@users.noreply.github.com>
2024-07-05 00:08:19 +0530

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -1,25 +1,24 @@
 import streamlit as st 
+from navigation import make_sidebar
 import pickle 
 import yaml 
 import pandas as pd
 
-# Load YAML config
+
 cfg = yaml.load(open("config.yaml", "r"), Loader=yaml.FullLoader)
 PKL_PATH = cfg['PATH']["PKL_PATH"]
 st.set_page_config(layout="wide")
 
-# Function to create sidebar
-from navigation import make_sidebar
+
+
 make_sidebar()
 
-# Load database from pickle
+#Load databse 
 with open(PKL_PATH, 'rb') as file:
     database = pickle.load(file)
 
-# Define columns for layout
-Index, Id, Name, Image = st.columns([0.5, 0.5, 3, 3])
+Index, Id, Name, Image  = st.columns([0.5,0.5,3,3])
 
-# Iterate through database items and display
 for idx, person in database.items():
     with Index:
         st.write(idx)
@@ -28,7 +27,4 @@ for idx, person in database.items():
     with Name:     
         st.write(person['name'])
     with Image:     
-        # Display image with custom CSS
-        st.markdown(f'<style>.stImage > img {{ max-width: 200px; }}</style>', unsafe_allow_html=True)
-        st.markdown(f'<div class="stImage"><img src="{person["image"]}" width="200"></div>', unsafe_allow_html=True)
-
+        st.image(person['image'],width=200)
